10eaa4e2f8c3e95e67bafe7aaacbada421f651b2
sunny <anna7789@naver.com>
2025-11-22 23:54:11 +0900

    fix: cors

--- app/fast_api/main.py
+++ app/fast_api/main.py
@@ -2,9 +2,17 @@
 from fastapi import FastAPI
 from app.core.database import get_mysql_connection
 from app.fast_api.routers.noise import router as noise_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 라우터 등록
 app.include_router(noise_router)
